19a3f595f19b6c5b661c0476d48ca7c58ce6f91b
rai8 <hytonne@outlook.com>
2020-09-25 20:29:49 +0300

    setting up webpack and webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebPlugin= require("html-webpack-plugin")
+
+module.exports={
+    module: {
+    rules:[
+        {
+            test: /\.html$/,
+            use: [
+                {
+                    loader: "html-loader",
+                    options: { minimize: true}
+                }
+            ]
+             }
+         ]
+    },
+    plugins: [
+        new HtmlWebPlugin({
+            template:  "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+
+}
